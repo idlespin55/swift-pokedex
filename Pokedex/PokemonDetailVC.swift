d5034213adbc1906c06c7372118cb2372722ce8a
idlespin55 <ajmal.ahmady@gmail.com>
2016-05-09 18:06:25 +0430

    Post Lesson 9

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Ajmal Ahmady on 5/9/16.
+//  Copyright © 2016 FrontierGroup. All rights reserved.
+//
+
+import Cocoa
+
+class PokemonDetailVC: UIViewController {
+
+}
