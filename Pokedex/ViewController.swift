751afbfee14ed609bf55fa847bd4172ed385cdc1
idlespin55 <ajmal.ahmady@gmail.com>
2016-05-09 10:56:03 +0430

    Task 7 Completed

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -5,28 +5,44 @@
 //  Created by Ajmal Ahmady on 4/29/16.
 //  Copyright © 2016 FrontierGroup. All rights reserved.
 //  UTILIZES COLLECTIONVIEWS
+//  TO USE NEW FONTS, DOWNLOAD, ADD TO PROJECT, & ADD NAME TO INFO.PLIST
 
 import UIKit
+import AVFoundation         //To play music
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     //CollectionViews need to implement three protocols: CVDataSource, CVDelegate, & CVDelegateFlowLayout
     //In cellForItemAtIndexPath(), we create a new pokeman instance and configure the cell
     
-    //**********IBOUTLETS**********
+    //**********IBOUTLETS & VARIABLES**********
     @IBOutlet weak var collection: UICollectionView!
     var pokeman = [Pokeman]()
+    var musicPlayer: AVAudioPlayer!
     
-
+    
+    //**********IBACTIONS**********
+    @IBAction func musicBtnPressed(sender: UIButton!) {
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
+    
+    
     //**********ViewDidLoad() IMPLEMENTATION**********
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.dataSource = self
         collection.delegate = self
         parsePokemonCSV()
+        initAudio()
     }
     
     
-    //**********FUNCTIONS**********
+    //**********SUPPORTING FUNCTIONS**********
     func parsePokemonCSV() {
         let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
         do {
@@ -39,6 +55,19 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
                 pokeman.append(poke)
             }
         }   catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    func initAudio() {
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        let pathURL = NSURL(string: path)
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: pathURL!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1      //Infinite loop
+            musicPlayer.play()
+        } catch let err as NSError {
             print(err.debugDescription)
         }
     }
