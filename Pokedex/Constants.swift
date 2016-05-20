0638349c50ccd008f47efd4383b590043697c575
idlespin55 <ajmal.ahmady@gmail.com>
2016-05-20 23:54:37 +0430

    Finished Part 13

--- Pokedex/Constants.swift
+++ Pokedex/Constants.swift
@@ -11,5 +11,4 @@ import Foundation
 //Store constants in a file.  If constants/variables are not in a class, they are globally accessible
 let URL_BASE = "http://pokeapi.co"
 let URL_POKEMON = "/api/v1/pokemon/"
-
 typealias DownloadComplete = () -> ()   //Define closure that accepts no parameters and returns nothing
--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -21,6 +21,8 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionTxt: String!
+    private var _nextEvolutionId: String!
+    private var _nextEvolutionLvl: String!
     private var _pokemonUrl: String!
     
     var name: String {
@@ -40,14 +42,87 @@ class Pokemon {
     
     func downloadPokemonDetails(completed: DownloadComplete) {
         let url = NSURL(string: _pokemonUrl)!
-        print(url)
-        print("1")
-
+        
         Alamofire.request(.GET, url).responseJSON { response in
-            print(response.request)  // original URL request
-            print(response.response) // URL response
-            print(response.data)     // server data
-            print(response.result)   // result of response serialization
+            //Download data
+            if let dict = response.result.value as? Dictionary<String, AnyObject> {
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+                if let height = dict["height"] as? String {
+                    self._height = height
+                }
+                if let attack = dict["attack"] as? Int {
+                    self._attack = "\(attack)"
+                }
+                if let defense = dict["defense"] as? Int {
+                    self._defense = "\(defense)"
+                }
+                
+                //Check downloaded data
+                print(self._weight)
+                print(self._height)
+                print(self._attack)
+                print(self._defense)
+                
+                //Parse data
+                if let types = dict["types"] as? [Dictionary<String, String>] where types.count > 0 {
+                    if let name = types[0]["name"] {
+                        self._type = name.capitalizedString
+                    }
+                    if types.count > 1 {
+                        for var x = 1; x < types.count; x++ {
+                            if let name = types[x]["name"] {
+                                self._type! += "/\(name.capitalizedString)"
+                            }
+                        }
+                    }
+                } else {
+                    self._type = ""
+                }
+                print(self._type)
+                if let descArr = dict["descriptions"] as? [Dictionary<String, String>] where descArr.count > 0 {
+                    if let url = descArr[0]["resource_uri"] {
+                        let nsurl = NSURL(string: "\(URL_BASE)\(url)")!
+                        Alamofire.request(.GET, nsurl).responseJSON { response in
+                            if let descDict = response.result.value as? Dictionary<String, AnyObject> {
+                                if let description = descDict["description"] as? String {
+                                    self._description = description
+                                    print(self._description)
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    self._description = ""
+                }
+                if let evolution = dict["evolutions"] as? [Dictionary<String, AnyObject>] where evolution.count > 0 {
+                    if let to = evolution[0]["to"] as? String {
+                        //Can't support mega pokemon right now but api still has mega data
+                        if to.rangeOfString("mega") == nil {
+                            if let uri = evolution[0]["resource_uri"] as? String {
+                                let newStr = uri.stringByReplacingOccurrencesOfString("/api/v1/pokemon/", withString: "")
+                                let num = newStr.stringByReplacingOccurrencesOfString("/", withString: "")
+                                self._nextEvolutionId = num
+                                self._nextEvolutionTxt = to
+                                if let lvl = evolution[0]["level"] as? Int {
+                                    self._nextEvolutionLvl = "\(lvl)"
+                                }
+                                print(self._nextEvolutionId)
+                                print(self._nextEvolutionTxt)
+                                print(self._nextEvolutionLvl)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            //POTENTIAL RESPONSES.  TYPICALLY ONLY NEED RESPONSE.RESULT.VALUE
+            //print(response.request)  // original URL request
+            //print(response.response) // URL response
+            //print(response.data)     // server data
+            //print(response.result)   // result of response serialization
+            //print(response.result.value)   // result of response serialization
         }
     }
 }
